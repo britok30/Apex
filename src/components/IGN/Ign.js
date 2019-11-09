2dfcb37be71aae4f9b9ea7c8aeab17c526c8c6fc
Kelvin Brito <britok30@gmail.com>
2019-11-09 13:10:34 -0500

    ign

--- src/components/IGN/Ign.js
+++ src/components/IGN/Ign.js
@@ -1,22 +1,22 @@
 import React, { Fragment, Component } from "react";
 import YouTube from "react-youtube";
+import "./Ign.css"
 
 class Ign extends Component {
   render() {
     const opts = {
-      height: "600",
-      width: "1140",
+      height: "390",
+      width: "600",
       playerVars: {
         autoplay: 0
       }
     };
     return (
       <Fragment>
-        <div style={{ margin: "3rem 0" }}>
-          <YouTube videoId="grJ1rKkLiZA" opts={opts} />
-        </div>
-        <div style={{ margin: "1rem 0" }}>
-          <YouTube videoId="jJ3N4LiNWwY" opts={opts} />
+        <div className="container youtube ">
+          <div>
+            <YouTube videoId="grJ1rKkLiZA" opts={opts} />
+          </div>
         </div>
       </Fragment>
     );
